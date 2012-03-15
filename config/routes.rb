5a9afafb25d60b2bbc3d99314d6a84dd988b772a
Kemi Ajiboye <ajiboye1@umbc.edu>
2012-03-15 15:05:16 -0400

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   #get "users/new"
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
     
     root :to => "pages#home"
 
